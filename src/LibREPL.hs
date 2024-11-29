dff25254bc0d33aab762950c2d569dd3bcbb1c87
Zerumi <afanasyev09032005@gmail.com>
2024-11-29 05:43:08 +0300

    feat: handle input errors

--- src/LibREPL.hs
+++ src/LibREPL.hs
@@ -7,10 +7,13 @@ module LibREPL
 where
 
 import Control.Monad (unless)
+import Data.Maybe
 import GHC.IO.Handle
 import GHC.IO.Handle.FD
 import LibEval
-import LibInterpolation
+import LibInterpolation hiding (window)
+import LibSlidingWindow
+import Text.Read
 
 -- | Read a line from the user
 read_ :: IO String
@@ -18,6 +21,19 @@ read_ =
   putStr "Enter point (x, y)> "
     >> hFlush stdout
     >> getLine
+
+-- | Validate the input
+validate_ :: String -> Bool
+validate_ str = case words str of
+  [x, y] -> isJust (readMaybe x :: Maybe Double) && isJust (readMaybe y :: Maybe Double)
+  _ -> False
+
+validateGrowingX :: String -> Interpolation -> Bool
+validateGrowingX input (Interpolation _ window _ _) = case words input of
+  [xStr, _] -> case head (getElements window) of
+    Just (x0, _) -> x0 < read xStr
+    _ -> True
+  _ -> False
 
 -- | Evaluate the input
 eval_ :: String -> Interpolation -> (Interpolation, Maybe String)
@@ -32,7 +48,12 @@ print_ (Just str) = putStrLn str
 loop_ :: [Interpolation] -> IO ()
 loop_ windows = do
   input <- read_
-  unless (input == ":quit") $ do
-    let (newWindows, results) = unzip $ map (eval_ input) windows
-    mapM_ print_ results
-    loop_ newWindows
+  unless (input == ":quit") $
+    if validate_ input && validateGrowingX input (head windows)
+      then do
+        let (newWindows, results) = unzip $ map (eval_ input) windows
+        mapM_ print_ results
+        loop_ newWindows
+      else do
+        putStrLn "Invalid input. Please enter a valid point (x, y). X should be greater than the previous X."
+        loop_ windows
